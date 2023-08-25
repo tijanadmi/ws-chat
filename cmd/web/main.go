21ced83fba69df4da571b88832756d0146d5a66a
Tijana Dmitrovic-Ilijev <tijanadmi@yahoo.com>
2023-08-25 14:53:56 +0200

    first commit

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/tijanadmi/ws-chat/internal/handlers"
+)
+
+func main() {
+	mux := routes()
+
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
+
+	log.Println("Staring web server on port 8080")
+
+	_ = http.ListenAndServe(":8080", mux)
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/bmizerany/pat"
+	"github.com/tijanadmi/ws-chat/internal/handlers"
+)
+
+// routes defines the application routes
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
